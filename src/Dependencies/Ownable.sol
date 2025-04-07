93b36288df352a2acb5670020a43fc0e3b4dea53
TanishqDsharma <tanishqsharma612@gmail.com>
2025-04-07 12:07:52 +0530

    Ownership contract

---
+++ src/Dependencies/Ownable.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+contract Ownable{
+    
+    // Store the owner's address
+    address private _owner;
+
+    // Event to track where Ownership is Transferred
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting `initialOwner` as the initial owner.
+     */
+    constructor(address initialOwner) {
+        _owner = initialOwner;
+        emit OwnershipTransferred(address(0), initialOwner);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+
+    function owner() public view returns(address){
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(isOwner(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Returns true if the caller is the current owner.
+     */
+    function isOwner() public view returns (bool) {
+        return msg.sender == _owner;
+    }
+
+     /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     *
+     * NOTE: This function is not safe, as it doesn’t check owner is calling it.
+     * Make sure you check it before calling it.
+     */
+    function _renounceOwnership() internal {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
+
+}
